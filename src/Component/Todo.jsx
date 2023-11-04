50c31cef8967cb063e24d8a4268fec2dc87ccfc8
Deepak <deepak2603dk@gmail.com>
2023-11-04 15:53:03 +0530

    adding delete function and fix bugs

--- src/Component/Todo.jsx
+++ src/Component/Todo.jsx
@@ -1,11 +1,20 @@
-import  { useEffect, useState } from "react";
-import './Todo.css'
-import {ImCross} from 'react-icons/im';
+import { useEffect, useState } from "react";
+import "./Todo.css";
+import { ImCross } from "react-icons/im";
 export default function Todo() {
   let [task, setTask] = useState("");
   let [desc, setDesc] = useState("");
-  let [data, setData] = useState([]);
 
+  const getLocalItems = () => {
+    let list = localStorage.getItem("data");
+    console.log(">>>>>>>>>>>", list);
+    if (list) {
+      return JSON.parse(localStorage.getItem("data"));
+    } else {
+      return [];
+    }
+  };
+  let [data, setData] = useState(getLocalItems());
 
   const taskhandler = (e) => {
     setTask(e.target.value);
@@ -16,44 +25,45 @@ export default function Todo() {
     console.log(">>>>>>>>>>>", desc);
   };
 
-useEffect(()=>{
-  let  todolist = JSON.stringify(data)
-  console.log('>>>>>>>>>>>', todolist)
-  localStorage.setItem('data',todolist)
+  useEffect(() => {
+    let todolist = JSON.stringify(data);
+    console.log(">>>>>>>>>>>", todolist);
+    localStorage.setItem("data", todolist);
+  }, [data]);
 
-  },[data])
-
+  const deleteHandler = (id) => {
+    const updatedData = data.filter((data, index) => {
+      return index != id;
+    });
+    setData(updatedData);
+  };
 
   const addhandler = () => {
-    if(task.trim()=='' || desc.trim()==''){
+    if (task.trim() == "" || desc.trim() == "") {
       return;
     }
     let obj = {
-        Task:task,
-        Desc:desc
-    }
-    setData([...data,obj])
+      Task: task,
+      Desc: desc,
+    };
+    setData([...data, obj]);
 
-    setTask('')
-    setDesc('')
-
- 
+    setTask("");
+    setDesc("");
   };
-
 
   return (
     <>
       <div className="container">
-          
-          <h5 className="heading">Todo APP</h5>
-          <input
-            type="text"
-            onChange={taskhandler}
-            value={task}
-            className="input my-2"
-            placeholder="Enter task here"
-          />
-          <div className="row  ">
+        <h5 className="heading">Todo APP</h5>
+        <input
+          type="text"
+          onChange={taskhandler}
+          value={task}
+          className="input my-2"
+          placeholder="Enter task here"
+        />
+        <div className="row  ">
           <input
             type="text"
             onChange={descHandler}
@@ -62,26 +72,26 @@ useEffect(()=>{
             placeholder="Enter description"
           />
         </div>
-        <button onClick={addhandler} >Add Task</button>
-        </div>
-        <hr />
-        <div className="screen  ">
-          
-            {
-                
-                data.map((data,index)=>(
-                      <div key={index} className="card ">
-                        <div className="cross">{<ImCross/>}</div>
-                        <h2>{data.Task}</h2>
-                        <h6>{data.Desc}</h6>
-                      </div>
-                ))
-
-            }
-        </div>
-                 
-
-
+        <button onClick={addhandler}>Add Task</button>
+      </div>
+      <hr />
+      <div className="screen  ">
+        {data.map((data, index) => (
+          <div key={index} className="card ">
+            <div className="cross">
+              {
+                <ImCross
+                  onClick={() => {
+                    deleteHandler(index);
+                  }}
+                />
+              }
+            </div>
+            <h2>{data.Task}</h2>
+            <h6>{data.Desc}</h6>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
